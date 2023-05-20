c8857d9c79b1cc5a59154676177c7034b317f185
moonholiday <727moonholiday@gmail.com>
2023-05-20 22:12:12 +0545

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React from 'react'
+import { Flex, Image } from '@chakra-ui/react'
+
+const Navbar: React.FC = () => {
+    return (
+        <Flex bg="white" height="44px" padding="6px 12px">
+            <Flex align='center'>
+                <Image src="/images/redditFace.svg" height='30px' />
+                <Image src="/images/redditText.svg" height='46px' />
+            </Flex>
+        </Flex>
+    );
+}
+export default Navbar
