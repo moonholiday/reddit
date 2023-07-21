f22ac57a5235f558bf9361def71740dc68ad2471
moonholiday <727moonholiday@gmail.com>
2023-07-21 15:22:18 +0545

    auth buttons

--- src/chakra/button.ts
+++ src/chakra/button.ts
@@ -1,42 +1,43 @@
-import { ComponentStyleConfig } from '@chakra-ui/theme'
+import { ComponentStyleConfig } from "@chakra-ui/theme";
 
 export const Button: ComponentStyleConfig = {
-    baseStyle: {
-        borderRadius: '60px',
-        fontSize: '10pt',
-        fontWeight: '700',
-        _focus: {
-            boxShadow: 'none',
-        }
+  baseStyle: {
+    borderRadius: "60px",
+    fontSize: "12pt",
+    fontWeight: "700",
+    margin: "8px",
+    _focus: {
+      boxShadow: "none",
     },
-    sizes: {
-        sm: {
-            fontSize: '8pt'
-        },
-        md: {
-            fontSize: '10pt'
-        }
+  },
+  sizes: {
+    sm: {
+      fontSize: "8pt",
     },
-    variants: {
-        solid: {
-            color: 'white',
-            bg: 'blue.500',
-            _hover: {
-                bg: 'blue.400',
-            }
-        },
-        outline: {
-            color: 'blue.500',
-            border: '1px solid',
-            borderColor: 'blue.500',
-        },
-        oauth: {
-            height: '34px',
-            border: '1px solid',
-            borderColor: 'gray.300',
-            _hover: {
-                bg: 'gray.50'
-            }
-        }
-    }
-}
+    md: {
+      fontSize: "10pt",
+    },
+  },
+  variants: {
+    solid: {
+      color: "white",
+      bg: "blue.500",
+      _hover: {
+        bg: "blue.400",
+      },
+    },
+    outline: {
+      color: "blue.500",
+      border: "1px solid",
+      borderColor: "blue.500",
+    },
+    oauth: {
+      height: "34px",
+      border: "1px solid",
+      borderColor: "gray.300",
+      _hover: {
+        bg: "gray.50",
+      },
+    },
+  },
+};
--- src/components/Modal/Auth/OAuthButton.tsx
+++ src/components/Modal/Auth/OAuthButton.tsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import { Flex, Button, Image } from '@chakra-ui/react'
+import React from "react";
+import { Flex, Button, Image, Text } from "@chakra-ui/react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/clientApp";
 
 const OAuthButtons: React.FC = () => {
+    const [SignInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     return (
-        <Flex direction='column' width="100%" mb={4}>
-            <Button variant='oauth'>
-                <Image src='/images/googlelogo.png' height='20px' mr={4} />
+        <Flex direction="column" width="100%" mb={4}>
+            <Button
+                variant="oauth"
+                isLoading={loading}
+                onClick={() => SignInWithGoogle}
+            >
+                <Image src="/images/googlelogo.png" alt="Google" height="20px" mr={4} />
                 Continue with Google
             </Button>
+            <div></div>
+            <Button variant="oauth">Some other provider</Button>
         </Flex>
-    )
-}
+    );
+};
 
-export default OAuthButtons
+export default OAuthButtons;
